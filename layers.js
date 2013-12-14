8a9348afdb7dc79f38df5770549c0d92e52868fe
Sudhakar <sudhakar82@gmail.com>
2013-12-14 23:14:28 +0530

    Updated Pin images

--- layers.js
+++ layers.js
@@ -1,35 +1,35 @@
 var schools = new L.LayerGroup();
-$.get('data/schools.json', function(data) {
-    _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'blue', 'fa')}).bindPopup(datum.school).addTo(schools);
+$.get('data/schools.json', function (data) {
+    _.each(data, function (datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIconImg('school.png')}).bindPopup(datum.school).addTo(schools);
     });
 });
 
 var hospitals = new L.LayerGroup();
-$.get('data/hospitals.json', function(data) {
-    _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude], {icon: getIcons('medkit', 'red', 'fa')}).bindPopup(datum.Name).addTo(hospitals);
+$.get('data/hospitals.json', function (data) {
+    _.each(data, function (datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIconImg('hospital.png')}).bindPopup(datum.name).addTo(hospitals);
     });
 });
 
 var subways = new L.LayerGroup();
-$.get('data/subways.json', function(data) {
-    _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude], {icon: getIcons('road', 'purple', 'fa')}).bindPopup(datum.name).addTo(subways);
+$.get('data/subways.json', function (data) {
+    _.each(data, function (datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIconImg('subway.png')}).bindPopup(datum.name).addTo(subways);
     });
 });
 
 var toilets = new L.LayerGroup();
-$.get('data/toilets.json', function(data) {
-    _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'red', 'fa')}).bindPopup(datum.area).addTo(toilets);
+$.get('data/toilets.json', function (data) {
+    _.each(data, function (datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIconImg('toilet.png')}).bindPopup(datum.area).addTo(toilets);
     });
 });
 
 var trafficSignals = new L.LayerGroup();
-$.get('data/trafficsignals.json', function(data) {
-    _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'red', 'fa')}).bindPopup(datum.area).addTo(trafficSignals);
+$.get('data/trafficsignals.json', function (data) {
+    _.each(data, function (datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIconImg('signal.png')}).bindPopup(datum.area).addTo(trafficSignals);
     });
 });
 
@@ -47,13 +47,17 @@ $.get('data/libraries.json', function(data) {
     });
 });
 
-var getIcons = function (iconName, color, prefix) {
-  return L.AwesomeMarkers.icon({
-       icon: iconName,
-       prefix: prefix,
-       iconSize: [38, 95],
-       markerColor: color
-  });
+function getIcons(iconName, color, prefix) {
+    return L.AwesomeMarkers.icon({
+        icon: iconName,
+        prefix: prefix,
+        iconSize: [38, 95],
+        markerColor: color
+    });
 };
+
+function getIconImg(iconName) {
+    return L.icon({iconUrl: 'images/' + iconName, shadowUrl: 'images/marker-shadow.png', iconAnchor: [0, 0], shadowAnchor: [0, 10]})
+}
 
 
