ae0a6e5a78bd66cba1a0d6dce927c060e4f987af
Sudhakar <sudhakar82@gmail.com>
2013-12-14 18:14:00 +0530

    Fixed the relative paths of data files

--- layers.js
+++ layers.js
@@ -1,33 +1,33 @@
 var schools = new L.LayerGroup();
-$.get('/data/schools.json', function(data) {
+$.get('data/schools.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.school).addTo(schools);
     });
 });
 
 var hospitals = new L.LayerGroup();
-$.get('/data/hospitals.json', function(data) {
+$.get('data/hospitals.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(hospitals);
     });
 });
 
 var subways = new L.LayerGroup();
-$.get('/data/subways.json', function(data) {
+$.get('data/subways.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(subways);
     });
 });
 
 var toilets = new L.LayerGroup();
-$.get('/data/toilets.json', function(data) {
+$.get('data/toilets.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.area).addTo(toilets);
     });
 });
 
 var trafficSignals = new L.LayerGroup();
-$.get('/data/trafficsignals.json', function(data) {
+$.get('data/trafficsignals.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.area).addTo(trafficSignals);
     });
