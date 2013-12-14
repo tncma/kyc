274d95ebd9bb65e1b63ad986f822b801a3769444
Sudhakar <sudhakar82@gmail.com>
2013-12-14 17:04:03 +0530

    Added hospitals layer

--- layers.js
+++ layers.js
@@ -6,10 +6,10 @@ $.get('/data/schools.json', function(data) {
     });
 });
 
-//var hospitals = new L.LayerGroup();
-//
-//$.get('/data/hospitals.json', function(data) {
-//    _.each(data, function(datum) {
-//        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.school).addTo(schools);
-//    });
-//});
+var hospitals = new L.LayerGroup();
+
+$.get('/data/hospitals.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(hospitals);
+    });
+});
