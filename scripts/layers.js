fd88779a6189e32b725a5a333738948a3d1be254
Sudhakar <sudhakar82@gmail.com>
2013-12-14 15:23:01 +0530

    Added schools overlay and positioned to chennai on load

---
+++ scripts/layers.js
@@ -0,0 +1,15 @@
+var schools = new L.LayerGroup();
+
+$.get('/data/schools.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.school).addTo(schools);
+    });
+});
+
+//var hospitals = new L.LayerGroup();
+//
+//$.get('/data/hospitals.json', function(data) {
+//    _.each(data, function(datum) {
+//        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.school).addTo(schools);
+//    });
+//});
