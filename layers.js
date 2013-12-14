018f1f986282f1cacd40a469c74ac0ef478a4767
Sudhakar <sudhakar82@gmail.com>
2013-12-14 17:58:20 +0530

    Added more data sets

--- layers.js
+++ layers.js
@@ -1,5 +1,4 @@
 var schools = new L.LayerGroup();
-
 $.get('/data/schools.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.school).addTo(schools);
@@ -7,9 +6,31 @@ $.get('/data/schools.json', function(data) {
 });
 
 var hospitals = new L.LayerGroup();
-
 $.get('/data/hospitals.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(hospitals);
     });
 });
+
+var subways = new L.LayerGroup();
+$.get('/data/subways.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(subways);
+    });
+});
+
+var toilets = new L.LayerGroup();
+$.get('/data/toilets.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.area).addTo(toilets);
+    });
+});
+
+var trafficSignals = new L.LayerGroup();
+$.get('/data/trafficsignals.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.area).addTo(trafficSignals);
+    });
+});
+
+
