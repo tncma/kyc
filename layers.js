bdaa9c4321f8c88e29fad278ecd8fca8023df97f
arunkumar <arunkum@thoughtworks.com>
2013-12-14 18:22:35 +0530

    Arun || adding some more icons

--- layers.js
+++ layers.js
@@ -1,36 +1,45 @@
 var schools = new L.LayerGroup();
 $.get('data/schools.json', function(data) {
     _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude], {icon: schoolMarker}).bindPopup(datum.school).addTo(schools);
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'blue', 'fa')}).bindPopup(datum.school).addTo(schools);
     });
 });
 
 var hospitals = new L.LayerGroup();
 $.get('data/hospitals.json', function(data) {
     _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(hospitals);
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('medkit', 'red', 'fa')}).bindPopup(datum.name).addTo(hospitals);
     });
 });
 
 var subways = new L.LayerGroup();
 $.get('data/subways.json', function(data) {
     _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.name).addTo(subways);
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('road', 'purple', 'fa')}).bindPopup(datum.name).addTo(subways);
     });
 });
 
 var toilets = new L.LayerGroup();
 $.get('data/toilets.json', function(data) {
     _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.area).addTo(toilets);
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'red', 'fa')}).bindPopup(datum.area).addTo(toilets);
     });
 });
 
 var trafficSignals = new L.LayerGroup();
 $.get('data/trafficsignals.json', function(data) {
     _.each(data, function(datum) {
-        L.marker([datum.latitude, datum.longitude]).bindPopup(datum.area).addTo(trafficSignals);
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'red', 'fa')}).bindPopup(datum.area).addTo(trafficSignals);
     });
 });
+
+var getIcons = function (iconName, color, prefix) {
+  return L.AwesomeMarkers.icon({
+       icon: iconName,
+       prefix: prefix,
+       iconSize: [38, 95],
+       markerColor: color
+  });
+};
 
 
