077fac47e9752777aede5b42f515f64390d6b539
unknown <Admin@INarunks.corporate.thoughtworks.com>
2013-12-14 19:21:49 +0530

    Added libraries and parks to layers

--- layers.js
+++ layers.js
@@ -33,6 +33,20 @@ $.get('data/trafficsignals.json', function(data) {
     });
 });
 
+var parks = new L.LayerGroup();
+$.get('data/parks.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'red', 'fa')}).bindPopup(datum.name).addTo(parks);
+    });
+});
+
+var libraries = new L.LayerGroup();
+$.get('data/libraries.json', function(data) {
+    _.each(data, function(datum) {
+        L.marker([datum.latitude, datum.longitude], {icon: getIcons('book', 'red', 'fa')}).bindPopup(datum.name).addTo(libraries);
+    });
+});
+
 var getIcons = function (iconName, color, prefix) {
   return L.AwesomeMarkers.icon({
        icon: iconName,
