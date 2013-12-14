49e9994d9f81e32aeb231182c6d537390e6d8c23
arunkumar <arunkum@thoughtworks.com>
2013-12-14 21:15:58 +0530

    Arungit status

--- R_Scripts/ConvertPincodesToLatLong.R
+++ R_Scripts/ConvertPincodesToLatLong.R
@@ -1,4 +1,4 @@
 pincodeToLatLong = read.table("IN.txt", sep='\t')
 data = read.csv("../data/hospitals.csv");
 finalData = merge(pincodeToLatLong,data,by.x="V2",by.y="PINCODE")
-write.table(finalData, file = "final_Data.csv")
+write.table(finalData, file = "final_Data.csv", sep = ",")
