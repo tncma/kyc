c889ad596d3a608e98de2a4f91664fcbfeea1b31
arunkumar <arunkum@thoughtworks.com>
2013-12-14 21:07:50 +0530

    Arun || adding lat long to hospitals csv

--- R_Scripts/ConvertPincodesToLatLong.R
+++ R_Scripts/ConvertPincodesToLatLong.R
@@ -1,4 +1,4 @@
 pincodeToLatLong = read.table("IN.txt", sep='\t')
-data = read.csv("Physical_infrastructure_facilities_in_Municipality_and_corporation_schools_in_Tamil_Nadu.csv");
-finalData = merge(pincodeToLatLong,data,by.x="V2",by.y="Pincode")
+data = read.csv("../data/hospitals.csv");
+finalData = merge(pincodeToLatLong,data,by.x="V2",by.y="PINCODE")
 write.table(finalData, file = "final_Data.csv")
