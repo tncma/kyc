729922f98634842fcb570756ffd4f00bb7054e21
Sudhakar <sudhakar82@gmail.com>
2013-12-15 12:31:53 +0530

    Wired up saving of comments

--- layers.js
+++ layers.js
@@ -4,11 +4,10 @@ schoolComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluste
 }});
 
 $.get('data/schools.json', function (data) {
-    _.each(data, function (datum) {
+    var schoolsData = fetch('schools', data);
+    _.each(schoolsData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('school.png')}).bindPopup(datum.school + commentsTemplate(datum.id, "schools")).addTo(schools);
-        if(datum.complaints != undefined) {
-            createComplaintsMarker(datum, "school-complaints").addTo(schoolComplaints);
-        }
+        addCommentsMarker(datum, schoolComplaints, "school-complaints");
     });
     schoolComplaints.addTo(schools);
 });
@@ -18,12 +17,12 @@ hospitalComplaints= new L.MarkerClusterGroup({iconCreateFunction: function(clust
     return new L.DivIcon({className: 'hospital-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
 $.get('data/hospitals.json', function (data) {
-    _.each(data, function (datum) {
+    var hospitalsData = fetch('hospitals', data);
+    _.each(hospitalsData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('hospital.png')}).bindPopup(datum.Name + commentsTemplate(datum.id, "hospitals")).addTo(hospitals);
-        if(datum.complaints != undefined) {
-            createComplaintsMarker(datum,"hospital-complaints").addTo(hospitals);
-        }
+        addCommentsMarker(datum, hospitalComplaints,"hospital-complaints");
     });
+    hospitalComplaints.addTo(hospitals);
 });
 
 var subways = new L.LayerGroup();
@@ -31,9 +30,12 @@ subwayComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluste
     return new L.DivIcon({className: 'subway-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
 $.get('data/subways.json', function (data) {
-    _.each(data, function (datum) {
+    var subwayData = fetch('subways', data);
+    _.each(subwayData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('subway.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "subways")).addTo(subways);
+        addCommentsMarker(datum, subwayComplaints, 'subway-complaints');
     });
+    subwayComplaints.addTo(subways);
 });
 
 var toilets = new L.LayerGroup();
@@ -41,9 +43,12 @@ toiletComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluste
     return new L.DivIcon({className: 'toilet-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
 $.get('data/toilets.json', function (data) {
-    _.each(data, function (datum) {
+    var toiletsData = fetch('toilets', data);
+    _.each(toiletsData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('toilet.png')}).bindPopup(datum.area + commentsTemplate(datum.id, "toilets")).addTo(toilets);
+        addCommentsMarker(datum, toiletComplaints, 'toilet-complaints');
     });
+    toiletComplaints.addTo(toilets);
 });
 
 var trafficSignals = new L.LayerGroup();
@@ -51,39 +56,51 @@ signalComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluste
     return new L.DivIcon({className: 'signal-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
 $.get('data/trafficsignals.json', function (data) {
-    _.each(data, function (datum) {
+    var trafficSignalsData = fetch('trafficSignals', data);
+    _.each(trafficSignalsData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('signal.png')}).bindPopup(datum.area + commentsTemplate(datum.id, "trafficSignals")).addTo(trafficSignals);
+        addCommentsMarker(datum, signalComplaints, 'signal-complaints');
     });
+    signalComplaints.addTo(trafficSignals);
 });
 
 var parks = new L.LayerGroup();
 parkComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
     return new L.DivIcon({className: 'park-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
-$.get('data/parks.json', function(data) {
-    _.each(data, function(datum) {
+$.get('data/parks.json', function (data) {
+    var parksData = fetch('parks', data);
+    _.each(parksData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('park.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "parks")).addTo(parks);
+        addCommentsMarker(datum, parkComplaints, 'park-complaints');
     });
+    parkComplaints.addTo(parks);
 });
 
 var libraries = new L.LayerGroup();
 libraryComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
     return new L.DivIcon({className: 'library-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
-$.get('data/libraries.json', function(data) {
-    _.each(data, function(datum) {
+$.get('data/libraries.json', function (data) {
+    var librariesData = fetch('libraries', data);
+    _.each(librariesData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('library.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "libraries")).addTo(libraries);
+        addCommentsMarker(datum, libraryComplaints, 'library-complaints');
     });
+    libraryComplaints.addTo(libraries);
 });
 
 var lakes = new L.LayerGroup();
 lakeComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
     return new L.DivIcon({className: 'lake-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
-$.get('data/lakes.json', function(data) {
-    _.each(data, function(datum) {
+$.get('data/lakes.json', function (data) {
+    var lakesData = fetch('lakes', data);
+    _.each(lakesData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('lake.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "lakes")).addTo(lakes);
+        addCommentsMarker(datum, lakeComplaints,'lake-complaints');
     });
+    lakeComplaints.addTo(lakes);
 });
 
 var corporationOffices = new L.LayerGroup();
@@ -91,14 +108,23 @@ officeComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluste
     return new L.DivIcon({className: 'office-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
 }});
 $.get('data/corporationoffices.json', function(data) {
-    _.each(data, function(datum) {
+    var corporationOfficesData = fetch('corporationOffices', data);
+    _.each(corporationOfficesData, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('corporation.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "corporationOffices")).addTo(corporationOffices);
+        addCommentsMarker(datum, officeComplaints, 'office-complaints');
     });
+    officeComplaints.addTo(corporationOffices);
 });
 
+function addCommentsMarker(datum, layerData, className) {
+    if (datum.comments != undefined) {
+        createComplaintsMarker(datum, className).addTo(layerData);
+    }
+}
+
 function createComplaintsMarker(datum, classname) {
-    return L.marker([datum.latitude, datum.longitude], {icon: L.divIcon({className: classname, html: datum.complaints.length, iconAnchor: [-5,-30]})})
-        .bindPopup(datum.complaints.join("<br/>"));
+    return L.marker([datum.latitude, datum.longitude], {icon: L.divIcon({className: classname, html: datum.comments.length, iconAnchor: [-5, -30]})})
+        .bindPopup(datum.comments.join("<br/>"));
 }
 
 function getIconImg(iconName) {
--- templates.js
+++ templates.js
@@ -2,8 +2,36 @@ var commentsTemplate = function (id, type) {
     return "<hr><div class='comments' data-id=" + id + " data-type= " + type + ">" +
         "Type: <select><options><option>Complaint</option><option>Suggestion</option></options></select><br>" +
         "<textarea rows=4 id='comment'></textarea><br>" +
-        "<button>Save</button>"+
+        "<button class='saveBtn'>Save</button>"+
         "</div>";
+}
+
+$(document).on('click', '.saveBtn', function(event) {
+    var btn = $(event.currentTarget);
+    el = btn.closest('.comments');
+    var type = el.data('type');
+    var id = el.data('id');
+    var comment = el.find('textarea').val();
+    var data = fetch(type);
+    var currentDatum = _.find(data, function (datum) {
+        return datum.id === id;
+    });
+    if(currentDatum.comments == undefined)
+        currentDatum.comments = [];
+    currentDatum.comments.push(comment);
+    save(type, data);
+    btn.replaceWith("Thank you!");
+});
+
+var save = function (name, value) {
+    localStorage[name] = JSON.stringify(value);
+}
+
+var fetch = function (name, defaultValue) {
+    if (localStorage[name] == undefined) {
+        save(name, defaultValue);
+    }
+    return JSON.parse(localStorage[name]);
 }
 
 var newMarker = function(){
