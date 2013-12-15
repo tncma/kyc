b4a82220f75f91736e7f34ff936fb1de34508d3f
unknown <Admin@INarunks.corporate.thoughtworks.com>
2013-12-15 11:21:48 +0530

    Changed textbox size

--- templates.js
+++ templates.js
@@ -1,7 +1,7 @@
 var commentsTemplate = function (id, type) {
     return "<hr><div class='comments' data-id=" + id + " data-type= " + type + ">" +
         "Type: <select><options><option>Complaint</option><option>Suggestion</option></options></select><br>" +
-        "<textarea id='comment'></textarea><br>" +
+        "<textarea rows=4 id='comment'></textarea><br>" +
         "<button>Save</button>"+
         "</div>";
 }
