fde2e2450828a1e1a3b440d055204f641015d9fc
Rajik <krajeswaribtech@gmail.com>
2013-12-15 12:29:07 +0530

    [GS/Raji] Clustering complaints

--- layers.js
+++ layers.js
@@ -1,24 +1,35 @@
 var schools = new L.LayerGroup();
+schoolComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'school-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
+
 $.get('data/schools.json', function (data) {
     _.each(data, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('school.png')}).bindPopup(datum.school + commentsTemplate(datum.id, "schools")).addTo(schools);
         if(datum.complaints != undefined) {
-            createComplaintsMarker(datum).addTo(schools);
+            createComplaintsMarker(datum, "school-complaints").addTo(schoolComplaints);
         }
     });
+    schoolComplaints.addTo(schools);
 });
 
 var hospitals = new L.LayerGroup();
+hospitalComplaints= new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'hospital-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/hospitals.json', function (data) {
     _.each(data, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('hospital.png')}).bindPopup(datum.Name + commentsTemplate(datum.id, "hospitals")).addTo(hospitals);
         if(datum.complaints != undefined) {
-            createComplaintsMarker(datum.complaints).addTo(hospitals);
+            createComplaintsMarker(datum,"hospital-complaints").addTo(hospitals);
         }
     });
 });
 
 var subways = new L.LayerGroup();
+subwayComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'subway-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/subways.json', function (data) {
     _.each(data, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('subway.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "subways")).addTo(subways);
@@ -26,6 +37,9 @@ $.get('data/subways.json', function (data) {
 });
 
 var toilets = new L.LayerGroup();
+toiletComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'toilet-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/toilets.json', function (data) {
     _.each(data, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('toilet.png')}).bindPopup(datum.area + commentsTemplate(datum.id, "toilets")).addTo(toilets);
@@ -33,6 +47,9 @@ $.get('data/toilets.json', function (data) {
 });
 
 var trafficSignals = new L.LayerGroup();
+signalComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'signal-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/trafficsignals.json', function (data) {
     _.each(data, function (datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('signal.png')}).bindPopup(datum.area + commentsTemplate(datum.id, "trafficSignals")).addTo(trafficSignals);
@@ -40,6 +57,9 @@ $.get('data/trafficsignals.json', function (data) {
 });
 
 var parks = new L.LayerGroup();
+parkComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'park-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/parks.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('park.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "parks")).addTo(parks);
@@ -47,6 +67,9 @@ $.get('data/parks.json', function(data) {
 });
 
 var libraries = new L.LayerGroup();
+libraryComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'library-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/libraries.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('library.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "libraries")).addTo(libraries);
@@ -54,6 +77,9 @@ $.get('data/libraries.json', function(data) {
 });
 
 var lakes = new L.LayerGroup();
+lakeComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'lake-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/lakes.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('lake.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "lakes")).addTo(lakes);
@@ -61,14 +87,17 @@ $.get('data/lakes.json', function(data) {
 });
 
 var corporationOffices = new L.LayerGroup();
+officeComplaints = new L.MarkerClusterGroup({iconCreateFunction: function(cluster) {
+    return new L.DivIcon({className: 'office-complaints', iconAnchor: [-5,-30], html: '<b>'+ cluster.getChildCount()+'</b>'})
+}});
 $.get('data/corporationoffices.json', function(data) {
     _.each(data, function(datum) {
         L.marker([datum.latitude, datum.longitude], {icon: getIconImg('corporation.png')}).bindPopup(datum.name + commentsTemplate(datum.id, "corporationOffices")).addTo(corporationOffices);
     });
 });
 
-function createComplaintsMarker(datum) {
-    return L.marker([datum.latitude, datum.longitude], {icon: L.divIcon({className: 'complaints', html: datum.complaints.length, iconAnchor: [-5,-30]})})
+function createComplaintsMarker(datum, classname) {
+    return L.marker([datum.latitude, datum.longitude], {icon: L.divIcon({className: classname, html: datum.complaints.length, iconAnchor: [-5,-30]})})
         .bindPopup(datum.complaints.join("<br/>"));
 }
 
